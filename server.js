f37831bff600c318e7bff403c2a623cd3c8f87be
kurlerprudent <149204888+kurlerprudent@users.noreply.github.com>
2023-12-18 09:22:05 +0000

    Added a patient model

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const app = express()
+const PORT = 3001
+mongoose.connect('mongodb://localhost:27017/patientdb', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+
+const patientSchema = new mongoose.Schema({
+    ID: {
+        type: Number,
+        required: true,
+      },
+    name: {
+      type: String,
+      required: true,
+    },
+    age: {
+      type: Number,
+      required: true,
+    },
+    gender: {
+      type: String,
+      enum: ['Male', 'Female', 'Other'],
+      required: true,
+    },
+    phoneNumber: {
+        type: Number,
+        required: true,
+      },
+    address: {
+      type: String,
+      required: true,
+    },
+  });
+
+  const Patient = mongoose.model('Patient', patientSchema);
+
+
+
+app.listen(PORT, () => {
+  console.log('Server started');
+});
+
